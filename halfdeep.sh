8bd6cf5949b01f79cb74dcc66005d02e5bd6b61e
Bob Harris <rsharris@bx.psu.edu>
2019-08-20 15:03:26 -0400

    ()

--- halfdeep.sh
+++ halfdeep.sh
@@ -14,64 +14,74 @@ gapFill=500K
 
 ref=$1
 ref=`echo $ref | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
+refbase=`basename $ref`
+refin=$1
 
-if [ ! -d $ref ]; then
-	mkdir $ref
+if [ ! -d halfdeep ]; then
+	echo "    halfdeep dir not found, has bam_depth not been run?"
+	exit -1
+fi
+
+if [ ! -d halfdeep/$refbase ]; then
+	echo "    halfdeep/$refbase dir not found, has bam_depth not been run?"
+	exit -1
 fi
 
 
-if [ -e $ref/scaffold_lengths.dat ]; then
-	echo "$ref/scaffold_lengths.dat found. Skip scaffold lengths step."
+if [ -e halfdeep/$refbase/scaffold_lengths.dat ]; then
+	echo "halfdeep/$refbase/scaffold_lengths.dat found. Skip scaffold lengths step."
 else
-	echo "Collecting scaffold lengths"
+	echo "Collecting scaffold lengths from $refin"
 	echo "\
-    fasta_lengths $1 > $ref/scaffold_lengths.dat"
-    fasta_lengths $1 > $ref/scaffold_lengths.dat
+    fasta_lengths $refin > halfdeep/$refbase/scaffold_lengths.dat"
+    fasta_lengths $refin > halfdeep/$refbase/scaffold_lengths.dat
 fi
 
 
-echo "Combining individual bam depths to single track, in short windows"
+echo "Combining individual reads-file depths to single track, in ${shortWindowSize} windows"
 
 cat input.fofn \
-  | while read bamName ; do
-	  gzip -dc $bamName.depth.dat.gz
+  | while read readsname ; do
+      readsname=`basename $readsname`
+      readsname=`echo $out | sed 's/.fasta.$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g' | sed 's/.fastq.gz$//g'`
+      gzip -dc $readsname.depth.dat.gz
 	  done \
   | awk '{ print $1,$2,$2,$3 }' \
   | genodsp --origin=one --uncovered:hide --precision=3 \
-	  --chromosomes=$ref/scaffold_lengths.dat \
+	  --chromosomes=halfdeep/$refbase/scaffold_lengths.dat \
 	  = sum --window=$shortWindowSize --denom=actual \
   | gzip \
-  > $ref/depth.dat.gz
+  > halfdeep/$refbase/depth.dat.gz
 
 echo "Computing percentiles of depth distribution"
 
-gzip -dc $ref/depth.dat.gz \
+gzip -dc halfdeep/$refbase/depth.dat.gz \
   | genodsp --origin=one --uncovered:hide --precision=3 --nooutput \
-	  --chromosomes=$ref/scaffold_lengths.dat \
+	  --chromosomes=halfdeep/$refbase/scaffold_lengths.dat \
 	  = percentile 40..60by10 --min=1/inf --report:bash \
-  > $ref/temp.percentile_commands
+  > halfdeep/$refbase/temp.percentile_commands
 
-cat $ref/temp.percentile_commands \
+cat halfdeep/$refbase/temp.percentile_commands \
   | grep "# bash command" \
   | tr "=" " " \
   | awk '{ print "export",$1"="$2 }' \
-  > $ref/percentile_commands.sh
+  > halfdeep/$refbase/percentile_commands.sh
 
-cat $ref/temp.percentile_commands \
+cat halfdeep/$refbase/temp.percentile_commands \
   | grep "# bash command" \
   | tr "=" " " \
   | awk '{ print "export","half"$1"="($2/2) }' \
   | sed "s/halfpercentile/halfPercentile/" \
-  >> $ref/percentile_commands.sh
+  >> halfdeep/$refbase/percentile_commands.sh
 
-rm $ref/temp.percentile_commands
+rm halfdeep/$refbase/temp.percentile_commands
 
 echo "Identifying half-deep intervals"
 
-source $ref/percentile_commands.sh
-gzip -dc $ref/depth.dat.gz \
+source halfdeep/$refbase/percentile_commands.sh
+gzip -dc halfdeep/$refbase/depth.dat.gz \
   | genodsp --origin=one --uncovered:hide --nooutputvalue \
-	  --chromosomes=$ref/scaffold_lengths.dat \
+	  --chromosomes=halfdeep/$refbase/scaffold_lengths.dat \
 	  = erase --keep:inside --min=${halfPercentile40} --max=${halfPercentile60} \
 	  = binarize \
 	  = dilate --right=$shortWindowSize-1 \
@@ -80,5 +90,5 @@ gzip -dc $ref/depth.dat.gz \
 	  = binarize \
 	  = dilate --right=$longWindowSize-1 \
 	  = close $gapFill \
-  > $ref/halfdeep.dat
+  > halfdeep/$refbase/halfdeep.dat
 
