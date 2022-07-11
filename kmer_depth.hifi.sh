d567fd9474804237844ffaad4fcf95dcfe71428e
Bob Harris <rsharris@bx.psu.edu>
2022-07-11 16:57:31 -0400

    .

--- kmer_depth.hifi.sh
+++ kmer_depth.hifi.sh
@@ -14,11 +14,10 @@ fi
 # FastK, subsample, and map_names are prerequisites
 
 
-#cpus=3  # allow user to set this?
-#cpus=$SLURM_CPUS_PER_TASK
 
 kmerSize=40     # allow user to set this?
 minAbundance=3  # allow user to set this?
+cpus=5          # allow user to set this?  OR cpus=$SLURM_CPUS_PER_TASK
 
 
 refFull=$1
@@ -29,8 +28,9 @@ if [ ! -e ${refFull} ]; then
 	exit -1
 fi
 
-ref=`echo ${refFull} | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fsa_nt$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g' | sed 's/.fsa_nt.gz$//g'`
+ref=`echo ${refFull} | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 refBase=`basename ${ref}`
+refExt=`echo ${refFull} | sed 's/^.*\.fasta$/.fasta/g' | sed 's/^.*\.fa$/.fa/g' | sed 's/^.*\.fasta.gz$/.fasta.gz/g' | sed 's/^.*\.fa.gz$/.fa.gz/g'`
 
 if [ ! -e input.fofn ]; then
 	echo "input.fofn does not exist. Exit."
@@ -65,7 +65,8 @@ fi
 
 sampleNum=` echo ${subsampleSpec} | tr "/" " " | awk '{ print $1 }'`
 numSamples=`echo ${subsampleSpec} | tr "/" " " | awk '{ print $2 }'`
-subsampleFofn=input.${sampleNum}_of_${numSamples}.fofn
+subsampleName=${sampleNum}_of_${numSamples}
+subsampleFofn=input.${subsampleName}.fofn
 
 echo "\
 cat input.fofn | subsample ${subsampleSpec} > ${subsampleFofn}"
@@ -84,10 +85,11 @@ fastqFiles=`cat ${subsampleFofn} \
       done`
 out=`echo ${fastqFiles} | awk '{ print $1 }'`
 out=`echo ${out} | sed 's/.fastq$//g' | sed 's/.fastq.gz$//g'`
+outBase=`basename ${out}`
 
 # make sure the final product has not already been computed
 
-finalProduct=halfdeep/${refBase}/kmer_profiles/${out}.depth.dat.gz
+finalProduct=halfdeep/${refBase}/kmer_profiles/${subsampleName}.depth.dat.gz
 
 if [ -e ${finalProduct} ]; then
 	echo "${finalProduct} found. Exit."
@@ -109,8 +111,8 @@ else
 	      done
 
 	echo "\
-	FastK -v -k${kmerSize} -t${minAbundance} -P${tmpDir} ${fastqFiles}"
-    time FastK -v -k${kmerSize} -t${minAbundance} -P${tmpDir} ${fastqFiles}
+	FastK -v -k${kmerSize} -t${minAbundance} -T${cpus} -P${tmpDir} ${fastqFiles}"
+    time FastK -v -k${kmerSize} -t${minAbundance} -T${cpus} -P${tmpDir} ${fastqFiles}
 fi
 
 if [ ! -e ${out}.ktab ]; then
@@ -124,38 +126,37 @@ fi
 
 # create k-mer contig profiles
 
-if [ -e ${outDir}/${refBase}.prof ]; then
-	echo "${outDir}/${refBase}.prof found. Skip FastK k-mer profile build."
+if [ -e ${outDir}/${refBase}.${subsampleName}.prof ]; then
+	echo "${outDir}/${refBase}.${subsampleName}.prof found. Skip FastK k-mer profile build."
 else
 	echo "Using FastK to build k-mer profiles"
 
     # create the symlink mentioned above
-	refBaseWithExt=`basename ${refFull}`
-	ln -s ../../${refFull} ${outDir}/${refBaseWithExt}
+	ln -s ../../${refFull} ${outDir}/${refBase}.${subsampleName}${refExt}
 
 	echo "\
     FastK -v -k${kmerSize} -p:${out}.ktab -P${tmpDir} ${outDir}/${refBaseWithExt}"
-    time FastK -v -k${kmerSize} -p:${out}.ktab -P${tmpDir} ${outDir}/${refBaseWithExt}
+    time FastK -v -k${kmerSize} -p:${out}.ktab -P${tmpDir} ${outDir}/${refBase}.${subsampleName}${refExt}
 fi
 
-if [ ! -e ${outDir}/${refBase}.prof ]; then
-	echo "Something went wrong with FastK k-mer profile build. ${outDir}/${refBase}.prof does not exist. Exit."
+if [ ! -e ${outDir}/${refBase}.${subsampleName}.prof ]; then
+	echo "Something went wrong with FastK k-mer profile build. ${outDir}/${refBase}.${subsampleName}.prof does not exist. Exit."
 	exit -1
 fi
 
-# convert k-mer contig profiles to same format as a bam depth file
+# convert k-mer contig profiles to the same format as a bam depth file
 
 cat ${ref}.names \
  | awk '{ print ++n,$1; }' \
- > ${outDir}/${refBase}.num_to_name
+ > ${outDir}/${refBase}.${subsampleName}.num_to_name
 
 echo "\
-Profex ${outDir}/${refBase}.prof 1-# ..."
-time Profex ${outDir}/${refBase}.prof 1-# \
+Profex ${outDir}/${refBase}.${subsampleName}.prof 1-# ..."
+time Profex ${outDir}/${refBase}.${subsampleName}.prof 1-# \
   | awk '/^Read/ { readNum = $2 } \
         !/^Read/ { if ((NF>0)&&(readNum != "")&&($2 > 0)) print readNum,$1,$2 }' \
   | tr -d ":" \
-  | map_names --key=1 ${outDir}/${refBase}.num_to_name \
+  | map_names --key=1 ${outDir}/${refBase}.${subsampleName}.num_to_name \
   | gzip \
   > ${finalProduct}
 
